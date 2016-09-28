f2200af522916c7e8bd2d271f7585a50dbc009b7
KongTu <kongkong@rice.edu>
2016-09-28 15:31:12 +0200

    initial commit

---
+++ MBtriggerEfficiency/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/MBtriggerEfficiency_pPb2016/MBtriggerEfficiency/',1)[0])+'/cfipython/slc6_amd64_gcc491/MBtriggerEfficiency_pPb2016/MBtriggerEfficiency')
