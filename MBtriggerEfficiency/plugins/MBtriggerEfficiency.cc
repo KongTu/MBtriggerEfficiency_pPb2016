fe3a9079bfd059a0936c1fe1e1c3fd403f6b41b0
Kong <kongPro@Kongs-MacBook-Pro-3.local>
2016-10-01 09:24:10 +0200

    add the trigger cross check back

--- MBtriggerEfficiency/plugins/MBtriggerEfficiency.cc
+++ MBtriggerEfficiency/plugins/MBtriggerEfficiency.cc
@@ -561,7 +561,7 @@ void MBtriggerEfficiency::analyze(const edm::Event& iEvent, const edm::EventSetu
    bool fireJet8_fromGT=false;
    bool fireJet12_fromGT=false;   
 
-/*
+
    int iErrorCode=-1;
    for (uint32_t iTr=0; iTr < trgList.size(); iTr++)
      {
@@ -612,7 +612,6 @@ void MBtriggerEfficiency::analyze(const edm::Event& iEvent, const edm::EventSetu
 //  std::cout<<"MENU USED: "<<m_l1GtUtils.l1TriggerMenu()<<std::endl;
 
      }
-   */
 
    if ((fireLongThr1||fireShortThr1)&&fireHF2) crossTest_HF1->Fill(0.1,1);
    if ((fireLongThr1||fireShortThr1)&&(!fireHF2)) crossTest_HF1->Fill(1.1,1);
