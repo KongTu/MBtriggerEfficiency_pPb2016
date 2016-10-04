0076fd91a04b21826329d283677da4997b89ecb1
KongTu <kongkong@rice.edu>
2016-10-04 12:21:30 +0200

    update config

--- MBtriggerEfficiency/test/mbtriggerefficiency_cfg.py
+++ MBtriggerEfficiency/test/mbtriggerefficiency_cfg.py
@@ -1,19 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("Demo")
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('Demo',eras.Run2_2016)
+
+#process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
 
 #'/store/data/Run2015D/ZeroBias/RAW/v1/000/259/152/00000/0C81667F-F672-E511-809D-02163E01440D.root'
-'/store/data/Run2016H/ZeroBias/RAW/v1/000/281/616/00000/10A691C2-3483-E611-AFD3-02163E012A9D.root'
-	)
+#'/store/data/Run2016H/ZeroBias/RAW/v1/000/281/616/00000/10A691C2-3483-E611-AFD3-02163E012A9D.root'
+'/store/data/Run2016H/ZeroBias/RAW/v1/000/281/616/00000/10A691C2-3483-E611-AFD3-02163E012A9D.root'	
+#'/store/data/Run2016D/ZeroBias/RAW/v2/000/276/582/00000/04805A7E-3247-E611-AF3C-02163E011B83.root'
+#'/store/data/Run2016H/ZeroBias/RAW/v1/000/281/638/00000/1EB0A26D-BF83-E611-A184-02163E012507.root'
+    )
 )
 
+
+process.load('Configuration.StandardSequences.Services_cff')
 process.load("Configuration.StandardSequences.Digi_cff")
 process.load('Configuration.StandardSequences.GeometryDB_cff')
 process.load("Configuration.StandardSequences.RawToDigi_cff")
@@ -42,13 +52,25 @@ process.es_pool = cms.ESSource("PoolDBESSource",
        connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'),
                authenticationMethod = cms.untracked.uint32(0)
 )
+
+#process.load('CondCore.CondDB.CondDB_cfi')
+#process.CondDB.connect = 'frontier://FrontierProd/CMS_CONDITIONS'
+#process.es_pool = cms.ESSource("PoolDBESSource",
+
+#	process.CondDB,
+#    		toGet = cms.VPSet(cms.PSet(
+#        	record = cms.string("HcalElectronicsMapRcd"),
+#        	tag = cms.string("HcalElectronicsMap_v7.05_offline")
+#    	))
+#)
+
 process.es_prefer_es_pool = cms.ESPrefer( "PoolDBESSource", "es_pool" )
 
 # override the L1 menu from an Xml file
 process.l1GtTriggerMenuXml = cms.ESProducer("L1GtTriggerMenuXmlProducer",
   TriggerMenuLuminosity = cms.string('startup'),
-  DefXmlFile = cms.string('L1Menu_Collisions2016_v7.xml'), 
-  #DefXmlFile = cms.string('L1Menu_Collisions2015_lowPU_25nsStage1_v8.xml'),
+  DefXmlFile = cms.string('L1Menu_Collisions2015_lowPU_v4_L1T_Scales_20141121.xml'), 
+  #DefXmlFile = cms.string('L1Menu_Collisions2016_v7_m2.xml'),
   VmeXmlFile = cms.string('')
 )
 process.L1GtTriggerMenuRcdSource = cms.ESSource("EmptyESSource",
@@ -60,8 +82,7 @@ process.es_prefer_l1GtParameters = cms.ESPrefer('L1GtTriggerMenuXmlProducer','l1
 
 
 process.load("Configuration.Geometry.GeometryDB_cff")
-process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
-
+process.load("Configuration.StandardSequences.RawToDigi_cff")
 process.load('L1Trigger.Configuration.L1Extra_cff')
 
 # replacing arguments for L1Extra
@@ -93,6 +114,8 @@ process.output = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('outProd.root')
 )
 
+process.TFileService = cms.Service("TFileService",fileName = cms.string("test.root"))
+
 process.trigsel = cms.EDFilter("HLTHighLevel",
      TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
      HLTPaths = cms.vstring('HLT_ZeroBias_v1'),
@@ -113,6 +136,7 @@ process.digian = cms.EDAnalyzer('MBtriggerEfficiency',
      	m_l1ForJetToken = cms.InputTag("l1extraParticles","Forward"),
 	m_l1TauJetToken = cms.InputTag("l1extraParticles","Tau"),
         hfDigiTag = cms.InputTag("hcalDigis"),
+        gtDigiToken = cms.InputTag("gtDigis"),
         gtDigiTag=cms.InputTag("gtDigis"),
 	gctDigiTag=cms.InputTag("gctDigis"),
 	caloTowerTag=cms.InputTag("towerMaker"),
@@ -122,5 +146,7 @@ process.digian = cms.EDAnalyzer('MBtriggerEfficiency',
 )
 
 process.p = cms.Path(process.hcalDigis + process.ecalDigis + process.ecalPreshowerDigis + process.gtDigis + process.gctDigis + process.L1Extra + process.digian)
+
+
 
 #process.ep = cms.EndPath(process.output)
